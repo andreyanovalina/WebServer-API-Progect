20f282ef1142570d088b9706440687aa38c081d7
Полина <polina8277@mail.ru>
2020-04-12 22:16:19 +0300

    Заполнение функционалом основного файла программы(main)

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, redirect, make_response, request, session, abort, jsonify
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from data import db_session
+from data.users import User
+from data.books import Books
+from registerform import RegisterForm
+from loginform import LoginForm
 from flask_restful import abort, Api
 
 
@@ -16,9 +20,68 @@ def main():
     app.run()
 
 
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        session = db_session.create_session()
+        user = session.query(User).filter(User.phone == form.phone.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    session = db_session.create_session()
+    return session.query(User).get(user_id)
+
+
 @app.route("/")
-def catalog():
-    return render_template("base.html", title='Интернет-магазин книг')
+def main_page():
+    session = db_session.create_session()
+    # if current_user.is_authenticated:
+    #     news = session.query(News).filter(
+    #         (News.user == current_user) | (News.is_private != True))
+    # else:
+    books = session.query(Books)
+    return render_template("main_page.html", books=books)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        session = db_session.create_session()
+        if session.query(User).filter(User.phone == form.phone.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            phone=form.phone.data
+        )
+        user.set_password(form.password.data)
+        session.add(user)
+        session.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
 
 
 if __name__ == '__main__':
